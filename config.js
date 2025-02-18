b3dd6ce10d5a3631e32b23228263802e622a325c
Yoshioka <email@example.com>
2025-02-18 22:57:00 +0530

    verify upd

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Constants
 //service-worker-cache v4.6.7
-const lastUpdated = "February 18, 2025 21:30 IST"; // Update dynamically in footer
+const lastUpdated = "February 18, 2025 22:30 IST"; // Update dynamically in footer
 
 //const SOCKET_URL = 'http://localhost:3000'; 
 const SOCKET_URL = 'https://nihongo-backend.onrender.com'; 
