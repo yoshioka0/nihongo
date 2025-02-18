8f089be6d041b8b4d8adad523314957c407c93dc
Yoshioka <email@example.com>
2025-02-18 11:32:37 +0530

    offline pg  v4.6.6

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Constants
-//service-worker-cache v4.6.5
-const lastUpdated = "February 18, 2025 11:15 IST"; // Update dynamically in footer
+//service-worker-cache v4.6.6
+const lastUpdated = "February 18, 2025 11:30 IST"; // Update dynamically in footer
 
 //const SOCKET_URL = 'http://localhost:3000'; 
 const SOCKET_URL = 'https://nihongo-backend.onrender.com'; 
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cache-v4.6.5';
+const CACHE_NAME = 'pwa-cache-v4.6.6';
 const OFFLINE_URL = '/nihongo/offline.html';
 
 const urlsToCache = [
